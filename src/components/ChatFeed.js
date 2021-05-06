33c870fb0316f35c2d704cf9a8f2a223624be899
ajalaj <ajalaj@AJALAJ-IN.oradev.oraclecorp.com>
2021-05-06 23:13:45 +0530

    Rendering messages in ChatFeed

--- src/components/ChatFeed.js
+++ src/components/ChatFeed.js
@@ -4,27 +4,62 @@ import MessageForm from './MessageForm'
 
 const ChatFeed = (props) => {
 
-    const {chats,activeChat, userName,messages}=props
+    const {chats,activeChat,userName,messages}=props
 
     const chat =chats && chats[activeChat];
 
-    // console.log(`current chat is  ${chat}, username  ${userName},${messages}`)
-    // console.log(props)
+    // console.log(`current chat is  ${chat}, userName  ${userName},${messages}`)
+    console.log(props)
 
     const renderMessages=()=>{
-
         //store keys of messages
         const keys=Object.keys(messages)
         return keys.map((key,index)=>{
             const message=messages[key]
-            const lastMessage
+            const lastMessageKey= index===0?null : keys[index-1]
+            const isMyMessage=userName===message.sender.username
+
+            return(
+                <div key={`msg_${index}`} style={{width:"100%"}}>
+                <div className="message-block">
+                {
+                    isMyMessage?
+                    <MyMessage message={message}/>:
+                    <TheirMessage message={message} lastMessage={lastMessageKey}/>
+    
+                }
+                </div>
+                <div className="read-receipts"  
+                style={{marginRight:isMyMessage?'18px':'0px', 
+                marginLeft:isMyMessage?'0px':'18px'}}>
+                read-receipts
+
+                </div>
+                </div>
+            )
         })
     }
     
     renderMessages()
 
+    if(!chat) return 'Loading..';
+
     return ( 
-        <div>Chatfeed</div>
+        <div className="chat-feed">
+        <div className="chat-title-container">
+        <div className="chat-title">{chat.title}</div>
+        <div className="chat-subtitle">
+        {chat.people.map((person)=>
+            `${person.person.username}`
+        )}</div>
+        </div>
+        {renderMessages()}
+        <div style={{height:'100px'}}/> 
+
+        <div className="message-form-container">
+             <MessageForm {...props} chatId={activeChat}/>
+        </div>
+        </div>
      );
 }
  
